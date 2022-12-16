1611c230e3cc6bc46dae8e2e9fb9207a33e5ee71
egidehirwa <egidehirwam@gmail.com>
2022-12-16 23:59:09 +0100

    Lesson about values and variables

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,4 +1,24 @@
 let js = 'amazing';
-if(js === 'amazing') alert('JS is FUN');
+// if(js === 'amazing') alert('JS is FUN');
 
 console.log(js);
+
+
+// Values and variables
+// Variables are named in camelcase
+// Variables connot be named using - reserved keywords (except "name"), starting numbers and special caracters
+
+// conventions - don't start a variable name with a capital letter
+// write constants (that never change) with all uppercase (ex: PI = 3,16)
+
+// name variables in a descriptive manner - for easier comprehension
+
+
+let firstName = "Bob"
+
+console.log(firstName);
+
+let myFirstJob = "Server";
+let myCurrentJob = "Programmer";
+
+console.log(myFirstJob);
